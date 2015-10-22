c87d8de67b7e8e056347dca467ade8f1d83d238d
perlancar (@pc-office) <perlancar@gmail.com>
2015-10-22 16:35:29 +0700

    Add module lists: Modules I'm avoiding & Test list

--- lib/Acme/CPANLists/PERLANCAR.pm
+++ lib/Acme/CPANLists/PERLANCAR.pm
@@ -5,6 +5,14 @@ package Acme::CPANLists::PERLANCAR;
 
 our @Module_Lists = (
     {
+        summary => 'Test list',
+        entries => [
+            {module=>'Foo::Bar', summary=>'bar'},
+            {module=>'Foo::Baz', summary=>'baz'},
+        ],
+    },
+
+    {
         summary => 'Random module reviews',
         entries => [
             {
@@ -36,6 +44,52 @@ _
             {module=>'V'},
         ],
     },
+
+    {
+        summary => "Modules I'm avoiding",
+        description => <<'_',
+
+This is a list of modules I'm currently avoiding to use in my code, for some
+reason.
+
+_
+        entries => [
+            {
+                module => 'Log::Any',
+                summary => 'Startup overhead',
+                description => <<'_',
+
+Since I write a lot of CLI scripts, many of which has self-tab-completion
+features (using `Perinci::CmdLine` framework), I prefer to use modules that have
+startup overhead that is as low as possible. Since 1.0+ release, `Log::Any`'s
+startup overhead increases significantly from < 1ms to about 10ms (this is by
+itself not a significant number but things add up for other modules and I'm
+trying to keep startup overhead under 25-50ms). The alternate module I'm using
+now `Log::Any::IfLOG` is a simple wrapper that avoids loading `Log::Any` unless
+some environment variable indicates that user wants logging.
+
+_
+                alternate_module => 'Log::Any::IfLOG',
+            },
+            {
+                module => 'File::Flock',
+                summary => 'Too many deps',
+                description => <<'_',
+
+I used to use `File::Flock` due to its simple interface. However, this module
+depends on things like `AnyEvent`, `Data::Structure::Util`, `File::Slurp`, et
+al, all of which seem unnecessary. Nowadays I'm opting to use
+File::Flock::Retry, or just plain `flock()`.
+
+_
+                alternate_module => 'File::Flock::Retry',
+            },
+            {
+                module => 'File::Slurp',
+                summary => 'Buggy',
+                alternate_module => 'File::Slurper',
+            },
+        ],
 );
 
 our @Author_Lists = (
