1384e3c7d4eeb2bffd0183dda64303718cb5d4fb
perlancar (@pc-office) <perlancar@gmail.com>
2015-11-20 16:12:31 +0700

    Add module list: Picking random lines from a file

--- lib/Acme/CPANLists/PERLANCAR.pm
+++ lib/Acme/CPANLists/PERLANCAR.pm
@@ -114,6 +114,41 @@ _
             {module=>'Sys::RunAlone', summary => 'Works by locking DATA handle'},
         ],
     },
+
+    {
+        summary => 'Picking random lines from a file',
+        entries => [
+            {
+                module=>'File::Random',
+                description => <<'_',
+
+The `random_line()` function from this module picks one or more random lines
+from a specified file. The whole content of the file need not be slurped into
+memory, but the routine requires a single-pass of reading all lines from the
+file. The algorithm is as described in perlfaq (See: `perldoc -q "random
+line"`).
+
+If you pick more than one lines, then there might be duplicates.
+
+_
+            },
+            {
+                module=>'File::RandomLine',
+                summary => 'Recommended for large files',
+                description => <<'_',
+
+This module gives you a choice of two algorithms. The first is similar to
+`File::Random`, giving each line of the file equal weight. The second algorithm
+is more interesting: lines are weighted by the number of characters in the
+previous line, due to the nature of the algorithm. The second algorithm works by
+random seeking the file, discard the line fragment, read the next line, then
+repeat the process until the desired number of lines is reached. This means one
+doesn't have to read the whole file. It might be preferred for very large files.
+
+_
+            },
+        ],
+    },
 );
 
 our @Author_Lists = (
