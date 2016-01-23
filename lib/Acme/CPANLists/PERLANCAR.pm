132d8fa1f5262f04389ea73cef7e148cc44e4cc8
perlancar (@pc-home) <perlancar@gmail.com>
2016-01-23 22:34:36 +0700

    Add module list: Retired modules

--- lib/Acme/CPANLists/PERLANCAR.pm
+++ lib/Acme/CPANLists/PERLANCAR.pm
@@ -167,6 +167,50 @@ _
             },
         ],
     },
+
+    {
+        summary => 'Retired modules',
+        description => <<'_',
+
+This is a list of some of the modules which I wrote but have now been retired,
+for various reasons but mostly because they are no longer necessary. I've
+purged/retired more modules than these (mostly failed experiments) but they are
+not worth mentioning here because nobody else seems to have used them.
+
+_
+        entries => [
+            {
+                module => 'Data::Schema',
+                description => <<'_',
+
+Superseded by `Data::Sah` since 2012.
+
+_
+                alternate_modules => ['Data::Sah'],
+            },
+            {
+                module => 'Carp::Always::Dump',
+                description => <<'_',
+
+Superseded by `Devel::Confess`, which can do color (`Carp::Always::Color`),
+dumps (`Carp::Always::Dump`), as well as a few other tricks, all in a single
+package.
+
+_
+                alternate_modules => ['Devel::Confess'],
+            },
+            {
+                module => 'Passwd::Unix::Alt',
+                description => <<'_',
+
+Abandoned this fork of `Passwd::Unix` due to a couple of fundamental issues and
+superseded by `Unix::Passwd::File` which is a clean-slate attempt.
+
+_
+                alternate_modules => ['Unix::Passwd::File'],
+            },
+        ],
+    },
 );
 
 our @Author_Lists = (
