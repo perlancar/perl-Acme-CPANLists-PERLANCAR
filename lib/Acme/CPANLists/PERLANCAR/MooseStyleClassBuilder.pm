086180c09d4f4b5f26b49141ea32b4e7bd880569
perlancar (@pc-home) <perlancar@gmail.com>
2016-03-26 08:59:59 +0700

    Add module lists: MooseStyleClassBuilder, NonMooseStyleClassBuilder, ArrayClassBuilder

---
+++ lib/Acme/CPANLists/PERLANCAR/MooseStyleClassBuilder.pm
@@ -0,0 +1,25 @@
+package Acme::CPANLists::PERLANCAR::MooseStyleClassBuilder;
+
+# DATE
+# VERSION
+
+our @Module_Lists = (
+    {
+        summary => 'Moose-style (Perl 6-style) class builders',
+        entries => [
+            {module => 'Class::Accessor',
+             summary => 'Supports basic form of "has" predicate'},
+            {module => 'Class::Accessor::Array::Alt',
+             summary => 'For array-backed objects, supports basic form of "has" predicate with some specific extensions'},
+            {module => 'Moo'},
+            {module => 'MooX::BuildClass',
+             summary => 'Utility to build Moo class at runtime'},
+            {module => 'Moos'},
+            {module => 'Moose'},
+            {module => 'Mouse'},
+        ],
+    },
+);
+
+1;
+# ABSTRACT:
---
+++ lib/Acme/CPANLists/PERLANCAR/NonMooseStyleClassBuilder.pm
@@ -0,0 +1,36 @@
+package Acme::CPANLists::PERLANCAR::NonMooseStyleClassBuilder;
+
+# DATE
+# VERSION
+
+our @Module_Lists = (
+    {
+        summary => 'Non-Moose-style class builders',
+        description => <<'_',
+
+This list catalogs class builders with interface that is different than the
+Moose family.
+
+See also a whole host of Class::Accessor::* modules.
+
+_
+        entries => [
+            {module => 'Class::Meta::AccessorBuilder',
+             summary=>'Part of the Class::Meta framework'},
+            {module => 'Class::Struct'},
+            {module => 'Class::Builder'},
+            {module => 'Object::Declare'},
+            {module => 'Object::Tiny'},
+            {module => 'Class::Tiny'},
+            {module => 'Object::New',
+             summary=>'Only provides a new() constructor method'},
+            {module => 'Class::Accessor',
+             summary => 'Also supports Moose-style "has" predicate'},
+            {module => 'Class::XSAccessor',
+             summary=>'Fast version of Class::Accessor, used by Moo'},
+        ],
+    },
+);
+
+1;
+# ABSTRACT:
