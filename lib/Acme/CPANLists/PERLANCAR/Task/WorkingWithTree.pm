99fe37e6266aa170b8be71672392e143153118f8
perlancar (@pc-office) <perlancar@gmail.com>
2016-03-29 17:02:20 +0700

    Add module list: Task::WorkingWithTree

---
+++ lib/Acme/CPANLists/PERLANCAR/Task/WorkingWithTree.pm
@@ -0,0 +1,62 @@
+package Acme::CPANLists::PERLANCAR::Task::WorkingWithTree;
+
+# DATE
+# VERSION
+
+my $text = <<'_';
+# Basics
+
+Perl classes to represent tree (node) structure: `Tree::Object` and
+`Tree::ObjectXS` (comes with several varieties). They provide methods like
+walking a tree, checking whether a node is the first child, getting sibling
+nodes, and so on.
+
+Perl modules to manipulate tree: `Data::Tree`, `Tree::Simple`.
+
+Memory-efficient tree nodes in Perl: `Tree::Node`.
+
+
+# Creating
+
+`Tree::FromStruct`, `Tree::FromText`, `Tree::FromTextLines`, `Tree::Create`,
+`Tree::Create::Size`, `Tree::FromFS`.
+
+`Data::Random::Tree`.
+
+
+# Visualizing as text
+
+`Tree::ToText`, `Tree::ToTextLines`.
+
+
+# Visualizing as graphic
+
+TODO
+
+
+# Other modules
+
+`Tree::ToFS`.
+
+Special kinds of trees: TODO.
+
+
+# Roles
+
+`Role::TinyCommons::Tree`.
+
+_
+
+our @Module_Lists = (
+    {
+        summary => 'Working with tree data structure in Perl',
+        description => $text,
+        tags => ['task'],
+        entries => [
+            map { +{module=>$_} } $text =~ /`(\w+(?:::\w+)+)`/g
+        ],
+    },
+);
+
+1;
+# ABSTRACT:
